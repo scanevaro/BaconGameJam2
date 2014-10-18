9cef8917a7582e1c071f8dbe596247fe72221d13
Elmar <elmarblabla@hotmail.com>
2014-10-18 02:10:01 +0200

    we can sail and shoot

--- core/src/com/deeep/jam/Game.java
+++ core/src/com/deeep/jam/Game.java
@@ -2,26 +2,42 @@ package com.deeep.jam;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 
 public class Game extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+    ShapeRenderer shapeRenderer;
+    SpriteBatch batch;
+    Texture img;
+    Ship ship;
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
+    @Override
+    public void create() {
+        shapeRenderer = new ShapeRenderer();
+        batch = new SpriteBatch();
+        img = new Texture("badlogic.jpg");
+        ship = new Ship();
+    }
+
+    @Override
+    public void render() {
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.begin();
+        batch.draw(img, 0, 0);
+        batch.end();
+        ship.update(Gdx.graphics.getDeltaTime());
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+        shapeRenderer.setColor(Color.BLUE);
+        shapeRenderer.rect(ship.x, ship.y, 10, 10, 20, 40, 1, 1, (float) Math.toDegrees(ship.rotation - Math.PI / 2));
+        shapeRenderer.setColor(Color.BLACK);
+        shapeRenderer.rect(ship.gun.x, ship.gun.y, 5, 5, 10, 20, 1, 1, (float) Math.toDegrees(ship.gun.rotation - Math.PI / 2));
+        for (Bullet bullet : ship.gun.bullets) {
+            shapeRenderer.rect(bullet.x, bullet.y, 5, 5, 2, 8, 1, 1, (float) Math.toDegrees(bullet.rotation - Math.PI / 2));
+        }
+        shapeRenderer.end();
+    }
 }
---
+++ core/src/com/deeep/jam/Ship.java
@@ -0,0 +1,56 @@
+package com.deeep.jam;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+
+/**
+ * Created by Elmar on 18-10-2014.
+ */
+public class Ship {
+    public float x, y;
+    public float rotation;
+    public float force;
+    public final float acceleration = 0.5f;
+    public final float friction = acceleration / 2;
+    public final float maxForce = 2;
+    public Gun gun;
+
+    public Ship() {
+        force = x = y = rotation = 0;
+        gun = new Gun();
+    }
+
+    public void update(float deltaT) {
+        if (force > deltaT * friction) {
+            force -= deltaT * friction;
+        } else if (force < -deltaT * friction) {
+            force += deltaT * friction;
+        } else {
+            force = 0;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.A)) {
+            rotation += deltaT * 2 * (force / maxForce);
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.D)) {
+            rotation -= deltaT * 2 * (force / maxForce);
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.W)) {
+            if (force < maxForce) {
+                force += deltaT * acceleration;
+            }
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.S)) {
+            if (force > maxForce / 5) {
+                force -= deltaT * acceleration / 2;
+            }
+        }
+        x += Math.cos(rotation) * force;
+        y += Math.sin(rotation) * force;
+        //System.out.println(this);
+        gun.update(this, deltaT);
+    }
+
+    public String toString() {
+        return "rot force (x, y)" + rotation + " " + force + " (" + x + ", " + y + ")";
+    }
+}
