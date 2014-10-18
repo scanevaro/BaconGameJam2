cab4457fe2a8e95dc8bd108bfb061360d7fb45a4
Elmar <elmarblabla@hotmail.com>
2014-10-18 15:47:32 +0200

    sprites

--- core/src/com/deeep/jam/entities/Gun.java
+++ core/src/com/deeep/jam/entities/Gun.java
@@ -1,12 +1,6 @@
 package com.deeep.jam.entities;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.deeep.jam.Game;
-import com.deeep.jam.classes.Shaking;
 
 import java.util.ArrayList;
 
@@ -14,19 +8,19 @@ import java.util.ArrayList;
  * Created by Elmar on 18-10-2014.
  */
 public abstract class Gun {
-
+    protected int level = 0;
+    protected boolean damaged = false;
     public float rotation;
     public float x, y;
     public ArrayList<Bullet> bullets = new ArrayList<Bullet>();
     public float bulletTimer = 0;
     public final float bulletSpeed = 0.5f;
-    protected TextureRegion textureRegion;
 
     public Gun() {
-        textureRegion = new TextureRegion(new Texture(Gdx.files.internal("gun.png")));
+
     }
 
-    public void update(Ship ship, float deltaT) {
+/*    public void update(Ship ship, float deltaT) {
         this.x = (float) (ship.x + Math.cos(ship.rotation) * 2);
         this.y = (float) (ship.y + Math.sin(ship.rotation) * 2);
         float deltaX = Gdx.input.getX() - x - 32;
@@ -42,18 +36,22 @@ public abstract class Gun {
             if (bulletTimer >= bulletSpeed) {
                 bulletTimer = 0;
                 bullets.add(new Bullet(x + 32, y + 22, rotation));
-                Game.shaking.addShake(new Shaking.Shake(0.2f,5));
+                Game.shaking.addShake(new Shaking.Shake(0.2f, 5));
             }
         }
         bulletTimer += deltaT;
-    }
+    }*/
 
-    public void render(SpriteBatch spriteBatch) {
+
+    public abstract void update(Ship ship, float deltaT);
+
+    public abstract void render(SpriteBatch spriteBatch);
+
+/*    public void render(SpriteBatch spriteBatch) {
         float theta = (float) Math.toRadians(rotation - Math.PI / 2);
         int compensationConstant = (int) (Math.PI * theta);
         spriteBatch.begin();
         spriteBatch.draw(textureRegion, x + 30, y + 12 - compensationConstant, 3, 0, 6, 14, 1, 1, (float) Math.toDegrees(rotation - Math.PI / 2));
         spriteBatch.end();
-    }
-
+    }*/
 }
--- core/src/com/deeep/jam/entities/guns/SmallCanon.java
+++ core/src/com/deeep/jam/entities/guns/SmallCanon.java
@@ -1,7 +1,11 @@
 package com.deeep.jam.entities.guns;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.deeep.jam.classes.Assets;
 import com.deeep.jam.entities.Gun;
+import com.deeep.jam.entities.Ship;
 
 /**
  * Created by Andreas on 10/18/2014.
@@ -11,13 +15,27 @@ public class SmallCanon extends Gun {
     TextureRegion[][] guns = new TextureRegion[3][2];
 
     public SmallCanon(int socketId) {
-        switch (socketId) {
-            case 0:
-        }
+        guns[0][0] = Assets.getAssets().getRegion("ship_gun_gray");
+        guns[0][1] = Assets.getAssets().getRegion("ship_gun_gray_destroyed");
+        guns[1][0] = Assets.getAssets().getRegion("ship_gun_green");
+        guns[1][1] = Assets.getAssets().getRegion("ship_gun_green_destroyed");
+        guns[2][0] = Assets.getAssets().getRegion("ship_gun_red");
+        guns[2][1] = Assets.getAssets().getRegion("ship_gun_red_destroyed");
     }
 
-    public void update() {
-
+    @Override
+    public void update(Ship ship, float deltaT) {
+        this.x = (float) (ship.x + Math.cos(ship.rotation) * 2);
+        this.y = (float) (ship.y + Math.sin(ship.rotation) * 2);
+        float deltaX = Gdx.input.getX() - x - 32;
+        float deltaY = Gdx.graphics.getHeight() - Gdx.input.getY() - y - 22;
+        rotation = (float) Math.atan2(deltaY, deltaX);
     }
 
+    @Override
+    public void render(SpriteBatch spriteBatch) {
+        spriteBatch.begin();
+        spriteBatch.draw(guns[level][(damaged) ? 1 : 0], x, y);
+        spriteBatch.end();
+    }
 }
