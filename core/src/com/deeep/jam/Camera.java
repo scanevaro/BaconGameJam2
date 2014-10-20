c17ffcc58a2f1250521b0d8d31bef4951a0adf25
Elmar van Rijnswou <elmarvanrijnswou@hotmail.com>
2014-10-20 11:18:10 +0200

    Works on android

--- core/src/com/deeep/jam/Camera.java
+++ core/src/com/deeep/jam/Camera.java
@@ -3,11 +3,11 @@ package com.deeep.jam;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.math.Matrix4;
+import com.badlogic.gdx.math.Rectangle;
 import com.deeep.jam.classes.Map;
 import com.deeep.jam.classes.Shaking;
 import com.deeep.jam.entities.Ship;
 
-import java.awt.*;
 
 /**
  * Created by Elmar on 19-10-2014.
@@ -50,7 +50,7 @@ public class Camera {
     public void update(float deltaT) {
         orthographicCamera.update();
         shaking.update(deltaT);
-        viewBounds.setLocation((int) (orthographicCamera.position.x - offSet - VIRTUAL_WIDTH / 2), (int) (orthographicCamera.position.y - offSet - VIRTUAL_HEIGHT / 2));
+        viewBounds.setPosition((int) (orthographicCamera.position.x - offSet - VIRTUAL_WIDTH / 2), (int) (orthographicCamera.position.y - offSet - VIRTUAL_HEIGHT / 2));
         if (ship != null) {
             orthographicCamera.position.set((int) (Math.min(Math.max((int) ship.x, Game.VIRTUAL_WIDTH / 2), Map.sizeX - Game.VIRTUAL_WIDTH) + shaking.getShakeX()),
                     (int) (Math.min(Math.max((int) ship.y, Game.VIRTUAL_HEIGHT / 2), Map.sizeY - Game.VIRTUAL_HEIGHT) + shaking.getShakeY()),
