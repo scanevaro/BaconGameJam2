e92341d4c539b351d0817da1b4a83d875f895cb7
Elmar van Rijnswou <elmarvanrijnswou@hotmail.com>
2014-11-12 15:23:34 +0100

    Controller

--- core/src/com/deeep/jam/entities/Ship.java
+++ core/src/com/deeep/jam/entities/Ship.java
@@ -154,12 +154,12 @@ public class Ship {
             }
             if (((Controller.getController().getMovementVector().angleRad())) < rotation) {
                 //if ((Math.abs(rotation - Controller.getController().getMovementVector().angleRad()) - Math.PI / 2) > 0)
-                    rotation += deltaT * (force / maxForce);
+                rotation -= deltaT * (force / maxForce);
                 System.out.println("-" + Controller.getController().getMovementVector().angleRad() + ", " + rotation);
                 //System.out.println("-- | " + (Controller.getController().getMovementVector().angleRad() - rotation));
             } else {
                 //if (Math.abs(rotation + Math.abs(Controller.getController().getMovementVector().angleRad()) - Math.PI / 2) > 0)
-                    rotation -= deltaT * (force / maxForce);
+                rotation += deltaT * (force / maxForce);
                 System.out.println("+" + Controller.getController().getMovementVector().angleRad() + ", " + rotation);
                 //System.out.println("++ | " + (Controller.getController().getMovementVector().angleRad() - rotation));
             }
